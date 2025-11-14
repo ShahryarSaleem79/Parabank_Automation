35b647119d253c039ba7770b3400bd36c3042ead
Shahryar <shahryar.saleem007@gmail.com>
2025-11-13 21:17:10 -0500

    Added log4j logger for logs

--- src/main/java/base/BaseTest.java
+++ src/main/java/base/BaseTest.java
@@ -7,6 +7,8 @@ import org.openqa.selenium.firefox.FirefoxDriver;
 import org.testng.annotations.AfterMethod;
 import org.testng.annotations.BeforeMethod;
 
+import utils.Log;
+
 public class BaseTest {
 	
 	protected WebDriver driver;
@@ -14,17 +16,23 @@ public class BaseTest {
 	//Setting Up things
 	@BeforeMethod
 	public void setUp() {
+		Log.info("Starting driver");
 		driver = new FirefoxDriver();
+		Log.info("Maximizing window");
 		driver.manage().window().maximize();
+		Log.info("fetching url");
 		driver.get("https://parabank.parasoft.com/parabank/index.htm");
+		Log.info("adding implicit wait");
 		driver.manage().timeouts().implicitlyWait(Duration.ofSeconds(5));
 	}
-//	@AfterMethod
+	
+	@AfterMethod
 	//Tearing down things
-//	public void tearDown() {
-//		if (driver!=null) {
-//			driver.close();
-//		}
-//	}
+	public void tearDown() {
+		if (driver!=null) {
+			Log.info("closing browser");
+			driver.close();
+		}
+	}
 
 }
--- src/test/java/tests/RegistrationPageTest.java
+++ src/test/java/tests/RegistrationPageTest.java
@@ -5,12 +5,15 @@ import org.testng.annotations.Test;
 
 import base.BaseTest;
 import page.RegistrationPage;
+import utils.Log;
 public class RegistrationPageTest extends BaseTest {
 	
 	@Test(description="Valid Registration")
 	public void RegisterValidUser() {
 		RegistrationPage register = new RegistrationPage(driver);
+		Log.info("clicking register button");
 		register.ClickRegisterBtn();
+		Log.info("Entering user information");
 		register.EnterFirstName("TestFirstName");
 		register.EnterLastName("TestLastName");
 		register.EnterAddress("1127 Textile Pl");
@@ -21,6 +24,7 @@ public class RegistrationPageTest extends BaseTest {
 		register.EnterSSN(987654321);
 		register.EnterPassword("Pass123");
 		register.EnterPasswordRepeat("Pass123");
+		Log.info("Clicking submit button");
 		register.ClickSubmitBtn();
 		System.out.println(driver.getTitle());
 		
